fc0977cfeb630f324bdbc21c124818472455a12b
Mike Potter <mikepotter32@users.noreply.github.com>
2015-02-06 14:24:42 -0700

    Update file.c
    check it out. Make sure it works, I haven't been able to test it, but it should work. Not sure if there's any more to do to fix it up, but if you have any suggestions just put them in.

--- fixed.c
+++ fixed.c
@@ -1,14 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 
 int main (int argc, char ** argv) {
-    FILE * fp;
-    char line[256];
+  int LINE_SIZE = 256;
+  FILE * fp;
+    char *line;
     int chars, words, lines;
     float avg_word_len, avg_line_len;
     int i;
 
-    chars = words = lines = 0;
+    line = malloc(LINE_SIZE * sizeof(char));
+    if (line == NULL){
+      printf("line malloc failed\n");
+      return 0;
+    }
+
+    words = lines = 0;
+
+    if (argv[1] == NULL){
+      printf("No file provided\n");
+      return 0;
+    }
+
     fp = fopen(argv[1], "r");
+    assert(fp != NULL && "File can't be opened");
 
     while (fgets(line, 255, fp) != NULL) {
         lines++;
@@ -25,8 +41,15 @@ int main (int argc, char ** argv) {
         }
     }
 
-    avg_word_len = (float) chars / words;
-    avg_line_len = (float) chars / lines;
+    fclose(fp);
+
+    if (words != 0 && lines != 0){
+      avg_word_len = (float) chars / words;
+      avg_line_len = (float) chars / lines;
+    } else {
+      avg_word_len = 0;
+      avg_line_len = 0;
+    }
 
     printf("Chars: %d\nWords: %d\tAvg Len: %.2f\n", chars, words, avg_word_len);
     printf("Lines: %d\tAvg Len: %.2f\n", lines, avg_line_len);
